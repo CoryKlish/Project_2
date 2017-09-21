8edccf92537bec3f776aa01f0f22dd4e6af5ef7e
Josh <jpinedainbox@gmail.com>
2017-09-21 19:26:29 -0400

    First commit

---
+++ main.c
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Joshua
+ *
+ * Created on August 21, 2017, 3:39 PM
+ */
+
+//New tester main
+#include <stdio.h>
+#include <stdlib.h>
+#define SIX 2*3
+
+/*
+ * 
+ */
+int main() {
+
+    int num;
+	num = 12 / SIX;
+	printf("The number is %d",num);
+    return (EXIT_SUCCESS);
+}
+
