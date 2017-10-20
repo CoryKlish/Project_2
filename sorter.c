2234a005d65287bf251452dcb26a89048dd6a4cf
Josh <jpinedainbox@gmail.com>
2017-10-20 17:22:08 -0400

    removing segfault

--- sorter.c
+++ sorter.c
@@ -21,7 +21,9 @@ int main(int argc, const char* argv[]) {
 	
 	//First argument should be -mode, indicating what to analyze
 	//Since argv[0] is the executable, use argv[1]
-	const char* inputmode = strdup(argv[1])	;	
+	const char* inputmode = strdup(argv[1])	;
+    if (inputmode == null)
+        printf("input mode is bad");
 	
 	//Third argument is optional directory mode
 	//Third argument is the -d symbol
