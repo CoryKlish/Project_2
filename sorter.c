803e1a034981153a83f964eaf55830bf9d9ccaf7
Josh <jpinedainbox@gmail.com>
2017-10-24 14:21:31 -0400

    modularizing

--- sorter.c
+++ sorter.c
@@ -9,54 +9,52 @@
 #include <sys/stat.h>
 #include "sorter.h"
 int main(int argc, char* argv[]) {
-    
-	if(argc - 1 < 2)
+    // Don't accept less than 2 arguments and more than 6 arguments
+	if(argc - 1 < 2 || argc - 1 > 7)
 	{
 		printf("Wrong number of arguments passed.");
 	}
 	
 	//First argument should be -mode, indicating what to analyze
 	//Since argv[0] is the executable, use argv[1]
-    //Third argument is optional directory mode
+    //Third argument INPUT directory
 	//Third argument is the -d symbol
-    char* inputmode = argv[1];
-    char * dir = argv[3];
-	
-	//checking if first argument is legitimate
-	int len = strlen(inputmode);
-	if (len > 2 || *(inputmode) != '-')
-	{
-		printf("First argument is not recognized, ending program");
-		exit(0);
-	}
+    //Fifth argument is the -o symbo
+    //Fifth Designates OUTPUT directory
     
-	//if directory option exists, check if legit
-    if (dir != NULL)
+    char* inputmode = VerifyMode(argv[1]);
+    
+    if (argc-1 > 2)
     {
-        len = strlen (dir);
-        if (len > 2 || *(dir) != '-')
+        char* dir = VerifyMode(argv[3]);
+        if (*dir == 'x')
         {
-            printf("Third argument is not recognized, ending program");
+            printf("\nInput Directory not recognized, exiting");
             exit(0);
         }
-        char dirMode = VerifyMode(*(dir + 1));
-        if(dirMode == 'x')
-            exit(0);
     }
+    
+    if (argc-1 > 4)
+    {
+        char* out = VerifyMode(argv[5]);
+        if (*dir == 'x')
+        {
+            printf("\nOutput Directory not recognized, exiting");
+            exit(0);
+        }
+    }
+    
+    
 	
-	//Verify mode against array of modes
-	// *inputmode+1 to get the character instead of hyphen
-	char mode = VerifyMode(*(inputmode + 1));
-	if(mode == 'x')
-		exit(0);
+	
 //////////////////Parsing first line for column types and testing user input//////////////////////////////////////////////////
-    int numFields = 0;
-    int* numP = &numFields; 
+ 
+    
+    //recordsize and line both for getline method
 	size_t recordsize;
 	char* line = NULL;
-
-	
 	size_t bytes = getline(&line, &recordsize, stdin);
+    
 	if (bytes == -1)
 	{
 		printf("\nEOF, ending program");
@@ -67,18 +65,18 @@ int main(int argc, char* argv[]) {
     len = strlen(line);
  
 
-    
+    //getSortType updates the number of fields, need a ptr to them
+    int numFields = 0;
+    int* numP = &numFields; 
+    //Validate user input (inputcol) against columns in the given csv
     char* inputCol = argv[2];
-    //evaluates the header, assume size of 40
     char* sortType = (char*)malloc(sizeof(char) * len);
     sortType = getSortType(line,inputCol,numP);
     
+ 
     int numRecords = 0;
     int* pNumRecords = &numRecords;
     Record* allrecords = createTable(pNumRecords,numFields); 
-
-
-    
     sort(sortType,numRecords,allrecords);
 	
 	return 0;
@@ -86,27 +84,29 @@ int main(int argc, char* argv[]) {
 	
 }//End main
 
-
-char VerifyMode(char mode)
+char VerifyMode(char* mode)
 {
-    const char* modes = "cd";
-	//If the mode == 'c' (first element in char* modes)
-	if (*(modes) == mode)
-	{
-		
-		return 'c';
-	}
-	if (*(modes + 1) == mode)
-	{
-		
-		return 'd';
-	}
-	//If a mode is entered that does not exist, return invalid char
-	else
-	{
-		printf("The sorter does not recognize the first argument -%c",mode);
-		return 'x';
-	}
-	
+    //list of possible modes in a char*
+    const char* modes = "cdo";
+    char vmode = NULL;
+    //for looping through the modes
+    int modeLen = strlen(modes);
+    int len = strlen(mode);
+    if (len > 2 || *(mode) != '-')
+    {
+        printf("\n %s is not recognized, ending program",mode);
+    }
+    
+	int i;
+    for (i = 0; i < modeLen; i++)
+    {
+        //if the mode in param == something in modes
+        if(*(mode+1) == *(modes + i))
+            vmode = (*(modes + i));
+    }
+    
+    if (vmode == NULL)
+        vmode = 'x';
+    
+    return vmode;
 }//End VerifyMode function
-
