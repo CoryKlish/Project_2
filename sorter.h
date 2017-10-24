fdc21fa452e14181b2ae6e95edb20dae61c450cb
Josh <jpinedainbox@gmail.com>
2017-10-24 14:26:38 -0400

    modularizing

--- sorter.h
+++ sorter.h
@@ -49,7 +49,7 @@ static void allocateToken(Record*, char*, int);
 static  char* getSortType(char* header,char* colName, int* numFields);
 static void sort (char* sortType, int numStructs, Record*);
 static void printStructs(Record list[], int numStructs);
-char VerifyMode(char mode);
+char VerifyMode(char* mode);
 Record* createTable(int* pNumRecords,int numFields);
 void mergeNum(Record list[], int left, int mid, int right,char* sortType);
 void sortNum(Record list[], int left, int right,char* sortType);
