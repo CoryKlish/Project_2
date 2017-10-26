1d451c8f49b1ffbde3972c1adbc0973c72331b81
Josh <jpinedainbox@gmail.com>
2017-10-26 10:35:45 -0400

    adding more to process directory

--- sorter.h
+++ sorter.h
@@ -49,7 +49,7 @@ static void allocateToken(Record*, char*, int);
 static  char* getSortType(char* header,char* colName, int* numFields);
 static void sort (char* sortType, int numStructs, Record*);
 static void printStructs(Record list[], int numStructs);
-static  DIR* getDirectory(char* path);
+DIR* getDirectory(char* path);
 DIR* processDirectory( DIR* directory);
 char VerifyMode(char* mode);
 Record* createTable(int* pNumRecords,int numFields);
