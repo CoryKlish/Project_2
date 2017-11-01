7d86da9189b8bb80dac345d8884d388ea87c8d1e
Josh <jpinedainbox@gmail.com>
2017-11-01 13:47:12 -0400

    lksdjf

--- sorter.h
+++ sorter.h
@@ -153,7 +153,7 @@ static int processDirectory(char* path, char* inputCol, char* outpath)
    
     struct dirent* entry;
     char* csv = ".csv";
-    int processCounter = 0;
+    int processCounter = 1;
     int len = strlen(path);
     
     
