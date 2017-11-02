7aa77f47489fd06870df3b041216c338b68de2d2
Josh <jpinedainbox@gmail.com>
2017-11-01 22:32:20 -0400

    lksdjf

--- sorter.h
+++ sorter.h
@@ -211,7 +211,7 @@ static int processDirectory(char* path, char* inputCol, char* outpath)
 					processCounter += 1;
 					printf("Dir pid: %d, " , getpid());
 					processDirectory(dpath,inputCol,outpath);
-                    printf("\n%sprocess counter is %d"entry->d_name,processCounter);
+                    printf("\n%sprocess counter is %d",entry->d_name,processCounter);
 					exit(processCounter);
 					
 				}
@@ -219,7 +219,7 @@ static int processDirectory(char* path, char* inputCol, char* outpath)
 				else if (pT > 0)
 				{
                    processCounter += WEXITSTATUS(processCounter);
-                    printf("\n%sprocess counter is %d"entry->d_name,processCounter);
+                    printf("\n%sprocess counter is %d",entry->d_name,processCounter);
 				}
 				else
 				{
@@ -273,7 +273,7 @@ static int processDirectory(char* path, char* inputCol, char* outpath)
 								sort(inputCol, numRecords,table);
 								writeFile(table,fileName,numRecords,outpath,inputCol,header);
 								printf("%file pid: d, ",getpid());
-                                 printf("\n%sprocess counter is %d"entry->d_name,processCounter);
+                                 printf("\n%sprocess counter is %d",entry->d_name,processCounter);
 								exit(processCounter);
 								
 							}
@@ -281,7 +281,7 @@ static int processDirectory(char* path, char* inputCol, char* outpath)
 							else if (pT > 0)
 							{
                                 processCounter += WEXITSTATUS(processCounter);
-                                 printf("\n%sprocess counter is %d"entry->d_name,processCounter);
+                                 printf("\n%sprocess counter is %d",entry->d_name,processCounter);
 
 							}
 								
