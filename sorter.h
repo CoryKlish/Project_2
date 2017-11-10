6577c8303744fcbbf10ed85f145725496bb250a4
Josh <jpinedainbox@gmail.com>
2017-11-10 01:07:19 -0500

    testing new changes

--- sorter.h
+++ sorter.h
@@ -203,6 +203,8 @@ static int processDirectory(char* path, char* inputCol, char* outpath, int flag)
 				int len = strlen(path);				
 				fflush(stdout);
 				int pT = fork();
+                processCounter++;
+
 				//in the child process, process the directory 
 				if (pT == 0)
 				{
@@ -212,7 +214,6 @@ static int processDirectory(char* path, char* inputCol, char* outpath, int flag)
 				//If we are the parent process,
 				else if (pT > 0)
 				{
-                   processCounter++;
 
                     //nothing goes on
 				}
@@ -271,6 +272,8 @@ static int processDirectory(char* path, char* inputCol, char* outpath, int flag)
 	   {		
 			if( (wait(&status)) > 0 )
 			{
+                processCounter++;
+
 			}
 			else
 			{
