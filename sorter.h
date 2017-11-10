4c94b82de949ab7519287c402910d8536b202121
Josh <jpinedainbox@gmail.com>
2017-11-10 00:39:46 -0500

    testing new changes

--- sorter.h
+++ sorter.h
@@ -203,6 +203,7 @@ static int processDirectory(char* path, char* inputCol, char* outpath, int flag)
 				int len = strlen(path);				
 				fflush(stdout);
 				int pT = fork();
+				processCounter++;
 				//in the child process, process the directory 
 				if (pT == 0)
 				{
