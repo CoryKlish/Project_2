baf8bac477403fa2579e0a3693174fec160b7b1a
Josh <jpinedainbox@gmail.com>
2017-11-28 10:04:34 -0500

    added a loop

--- testgen.py
+++ testgen.py
@@ -3,23 +3,27 @@
 import csv
 import os
 from random import *
+loops = 10
+for i in range(loops):
 
-#Word to Daniel Chen on Piazza 
+    #Word to Daniel Chen on Piazza 
 
-inputFile = "movie_metadata.csv"
+    inputFile = "movie_metadata.csv"
 
-numFiles = int(input("Number of files to create: "))
-numRows = int(input("Number of rows between 1 and 5043: "))
+    numFiles = int(input("Number of files to create: "))
+    numRows = int(input("Number of rows between 1 and 5043: "))
 
-os.mkdir(str(numFiles))
-path = "./" + str(numFiles) + "/"
+    os.mkdir(str(numFiles))
+    #This creates directory name of the new created directory
+    path = "./" + str(numFiles) + "/"
 
-reader = csv.reader(open(inputFile, 'r'))
-reader = list(reader)
 
-#write all csv files
-for i in range(numFiles):
-    writer = csv.writer(open(path +'metadata_{}.csv'.format(i), 'w'))
-    writer.writerow(reader[0]) #category row
-    for i in range(numRows):
-        writer.writerow(reader[randint(1,5043)])
+    reader = csv.reader(open(inputFile, 'r'))
+    reader = list(reader)
+
+    #write all csv files
+    for i in range(numFiles):
+        writer = csv.writer(open(path +'metadata_{}.csv'.format(i), 'w'))
+        writer.writerow(reader[0]) #category row
+        for i in range(numRows):
+            writer.writerow(reader[randint(1,5043)])
