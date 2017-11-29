0d4fda3a61f34a73fb6cc66fe57ac8ef538ddbbd
Josh <jpinedainbox@gmail.com>
2017-11-29 00:03:27 -0500

    nchange row = NULL

--- mergesort.c
+++ mergesort.c
@@ -42,7 +42,7 @@ Record* createTable(int* pNumRecords, int numFields, FILE *fp)
 	while (bytes != -1)
 	{
 		//copy to row to free up the line var
-		char* row = malloc(sizeof(char) * strlen(line)); 
+		char* row = NULL; 
         row = strdup(line);
 		free(line);
 		line = NULL;
