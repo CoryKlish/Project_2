95ac3ec3d3c708fc014fd27200a3c459637d3e1d
Josh <jpinedainbox@gmail.com>
2017-11-29 10:00:51 -0500

    realloc after creating a table

--- mergesort.c
+++ mergesort.c
@@ -161,7 +161,7 @@ Record* createTable(int* pNumRecords, int numFields, FILE *fp)
  
 	}//end while
     
-    arSize = *pNumRecords
+    arSize = *pNumRecords + 1;
     //reallocate, move pointer to new memory location with more mem
     allrecords = (Record*)realloc(allrecords, arSize);
     if ( allrecords== NULL)
