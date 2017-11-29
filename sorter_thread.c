0f68a487679c140b25c042d3f40cb683e4d603cb
Cory J. Klish <cjk160@cp.cs.rutgers.edu>
2017-11-28 19:02:50 -0500

    yay

--- sorter_thread.c
+++ sorter_thread.c
@@ -10,7 +10,11 @@
 #include "sorter_thread.h"
 
 int main(int argc, char** argv) {
-
+	
+	header = strdup("color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,\
+movie_title,num_voted_users,cast_total_facebook_likes,actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,\
+title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes");
+	
 	int numArgs = argc - 1;
 	int retval;
 ////////////////////////////////////Verifying the input arguments////////////////////////////////////    
@@ -61,8 +65,6 @@ int main(int argc, char** argv) {
 		exit(0);
 	}
 	//initialize kahunaComp with 1024 spaces for array
-	
-	
 	else
 	{
         //====Init=====
--- sorter_thread.h
+++ sorter_thread.h
@@ -59,9 +59,7 @@ static pthread_mutex_t runningThreadLock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t kahunaCompLock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t rpLock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t cvlock = PTHREAD_MUTEX_INITIALIZER;
-static char* header = "color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,\
-movie_title,num_voted_users,cast_total_facebook_likes,actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,\
-title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes";
+static char* header = NULL;
 
 //=========Thread Id Section=========
 static pthread_t* tidArray;
@@ -499,7 +497,7 @@ static void *getFile(void* params)
     //readfile validates the input column and creates a record array
     //char** pheader will change the value of 
     //char* header from within readFile
-    char* header;
+    
     char** pHeader = &header;
   
     
