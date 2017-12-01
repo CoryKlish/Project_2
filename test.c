572da008b3a252741e848ea95ef7fea38015e3c5
Josh <jpinedainbox@gmail.com>
2017-12-01 10:21:45 -0500

    test

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char* string = "This, is , a , string.";
+    char *field = strtok(string,",");
+    printf("%u, %u\n",string, field);
+
+}
